757b38f9151e8ad89f3b3882a86c5351e7867221
Pratik Shekhar <pratikshekhar26@gmail.com>
2019-03-10 15:24:54 -0400

    Pictures of recipies along with title are being displayed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Form from './Components/Form';
+import apiKey from './Constant.js';
 
 class App extends Component {
+
+  state = {
+    cravings: []
+  }
+
+  getCravings = async (e) => {
+    const recipeName = e.target.elements.recipeName.value;
+    e.preventDefault();
+    const apiCall = await fetch(`https://www.food2fork.com/api/search?key=${apiKey}&q=${recipeName}&count=5`);
+    const data = await apiCall.json();
+    this.setState({cravings: data.recipes});
+    console.log(this.state.cravings);
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+            <h1 className="App-title">Happiness is Home Made Food</h1>
+            <Form  getCravings={this.getCravings} />
+            <div className = "container">
+                <div className = "row">
+                     {this.state.cravings.map((recipe)=>{
+                        return (
+                            <div key={recipe.title} className="col-md-4" style = {{marginBottom:"2rem"}}>
+                                <div className="craving__box">
+                                    <img className="craving__box-img" src={recipe.image_url} alt={recipe.title} />
+                                    <div className="craving__text">
+                                        <h5 className="craving__title">{recipe.title}</h5>
+                                    </div>
+                                    <button className="craving__buttons">View Recipe</button>
+                                </div>
+                            </div>
+                        );
+                    })}
+                </div>
+            </div>
         </header>
       </div>
     );
---
+++ src/Constant.js
@@ -0,0 +1,7 @@
+
+
+//const apiKey = "eeb1e15a51abd66394568f5f60bedfca";
+
+const apiKey = "c2785eea2057ed5e73c507ece4189bdc";
+
+export default apiKey;
